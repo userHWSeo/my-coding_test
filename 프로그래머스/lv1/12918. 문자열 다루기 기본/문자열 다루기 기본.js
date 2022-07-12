cf9bca5ed36050dfb1f23dc425273ef03d51f299
heewon <gmldnjseocnd@naver.com>
2022-07-12 18:59:11 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,9 +1,7 @@
 function solution(s) {
     var answer = false;
     if(s.length === 4 || s.length === 6){
-        if(parseInt(s) == s){
-            answer = true;
-        } 
+        answer = parseInt(s) == s
     }
     return answer;
 }
