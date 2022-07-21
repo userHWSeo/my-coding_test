acdd0f549c2e0542c575fe73c802885d767da8fd
heewon <gmldnjseocnd@naver.com>
2022-07-22 00:32:40 +0900

    [level 1] Title: 모의고사, Time: 4.54 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -3,16 +3,10 @@ function solution(answers) {
     let firStudent = [1, 2, 3, 4, 5];
     let secStudent = [2, 1, 2, 3, 2, 4, 2, 5];
     let thiStudent = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-    const counts = {
-        firCount: 0,
-        secCount: 0,
-        thiCount: 0,
-    }
     let firInt = 0;
     let secInt = 0;
     let thiInt = 0;
-    let correctAnswer = [];
-    let overlap = 0;
+    let correctAnswer = [0,0,0];
     // 정답의 길이만큼 반복
     for(let i = 0; i < answers.length; i++){
         // answer의 길이가 1회 반복보다 길면 각 int들을 다시 0으로 보내어 처음부터 카운트
@@ -27,22 +21,17 @@ function solution(answers) {
         }
         // answers들의 정답 횟수를 counts 객체에 저장
         if(answers[i] === firStudent[firInt]){
-            counts.firCount = (counts.firCount || 0) +1
+            correctAnswer[0] = (correctAnswer[0] || 0) +1
         }
         if(answers[i] === secStudent[secInt]){
-            counts.secCount = (counts.secCount || 0) +1
+            correctAnswer[1] = (correctAnswer[1] || 0) +1
         }
         if(answers[i] === thiStudent[thiInt]){
-            counts.thiCount = (counts.thiCount || 0) +1
+            correctAnswer[2] = (correctAnswer[2] || 0) +1
         }
         firInt++;
         secInt++;
         thiInt++;
-    }
-    console.log(counts)
-    // key의 value를 배열로 전환
-    for(val in counts){
-       correctAnswer.push(counts[val])
     }
     // 배열 중 가장 큰 수 찾기
     const maxValue = Math.max(...correctAnswer)
