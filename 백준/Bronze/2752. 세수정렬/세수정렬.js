0348d0ce7fc1636a15bc3fa78d7cee4401b94d23
heewon <gmldnjseocnd@naver.com>
2023-05-27 22:55:14 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let arr = input[0].split(" ").map(Number);
+
+arr.sort((a, b) => a - b);
+
+let answer = "";
+
+for (let i = 0; i < arr.length; i++) {
+  answer += arr[i] + " ";
+}
+
+console.log(answer);
